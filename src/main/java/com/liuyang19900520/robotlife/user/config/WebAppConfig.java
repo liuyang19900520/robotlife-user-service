bfdf360e928e283183867ea10072b3611080df9c
liuyang19900520 <liuyang19900520@hotmail.com>
2018-09-06 19:42:19 +0900

    WebAppConfig V0.2

--- src/main/java/com/liuyang19900520/robotlife/user/config/WebAppConfig.java
+++ src/main/java/com/liuyang19900520/robotlife/user/config/WebAppConfig.java
@@ -1,23 +1,98 @@
 package com.liuyang19900520.robotlife.user.config;
 
-import com.liuyang19900520.robotlife.user.exception.SysExceptionHandler;
-import org.springframework.web.servlet.HandlerExceptionResolver;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.liuyang19900520.robotlife.user.common.util.json.JsonObjectMapper;
+import com.liuyang19900520.robotlife.user.common.util.json.spring.JsonReturnHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.math.BigInteger;
 import java.util.List;
 
 /**
- * @program: robotlife-user-service
+ * @program: robotlife-blog-java
  * @description:
  * @author: LiuYang
- * @create: 2018-08-30 16:34
+ * @create: 2018-07-13 17:06
  **/
+@Configuration
 public class WebAppConfig implements WebMvcConfigurer {
 
+    /**
+     * 跨域
+     *
+     * @param registry
+     */
     @Override
-    public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
-        resolvers.add(new SysExceptionHandler());
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
+    /**
+     * null 转 ""
+     *
+     * @param converters
+     */
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter jackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = jackson2HttpMessageConverter.getObjectMapper();
+        //不显示为null的字段
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        objectMapper.registerModule(simpleModule);
+
+        jackson2HttpMessageConverter.setObjectMapper(objectMapper);
+        //放到第一个
+        converters.add(0, jackson2HttpMessageConverter);
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter registerConverter() {
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = new JsonObjectMapper();
+        SimpleModule simpleModule = new SimpleModule();
+
+        /**
+         *  将Long,BigInteger序列化的时候,转化为String
+         */
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        simpleModule.addSerializer(BigInteger.class, ToStringSerializer.instance);
+
+        objectMapper.registerModule(simpleModule);
+
+        messageConverter.setObjectMapper(objectMapper);
+
+        return messageConverter;
+    }
+
+    /**
+     * json注解
+     *
+     * @param handlers
+     */
+    @Override
+    public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> handlers) {
+        handlers.add(new JsonReturnHandler());
     }
 
 
+    @Bean(name = "multipartResolver")
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(500 * 1024 * 1024);
+        return multipartResolver;
+    }
 }
